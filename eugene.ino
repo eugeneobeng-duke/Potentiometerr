785716c48bbf58c74bead4c9eef80eee3583bc23
eugeneobeng-duke <eugeneobeng1628@gmail.com>
2025-07-17 23:59:22 +0000

    Add files via upload

---
+++ eugene.ino
@@ -0,0 +1,18 @@
+ const int potPin = 25;
+const int ledPin = 14;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  int potValue = analogRead(potPin);
+  int brightness = map(potValue, 0, 4095, 0, 255);
+  analogWrite(ledPin, brightness);
+
+  Serial.print("Potentiometer Value: ");
+  Serial.println(potValue);
+
+  delay(500);
+}
